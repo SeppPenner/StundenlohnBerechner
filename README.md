4a806171486f48a02d7f0743af77fbc44082a83d
tim <jan-brenner@gmx.de>
2017-03-17 20:03:50 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+StundenlohnBerechner
+====================================
+
+StundenlohnBerechner is an executable to calculate the net hourly salary.
+The executable was written and tested in .Net 4.6.2.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/dto3h4jgaf6305s9?svg=true)](https://ci.appveyor.com/project/SeppPenner/stundenlohnberechner)
+
+
+Change history
+--------------
+
+* **Version 1.0.0.1 (2017-03-17)** : Switched to .Net to 4.6.2, Refactored code.
+* **Version 1.0.0.0 (2017-03-17)** : 1.0 release.
---
+++ StundenlohnBerechner/Main.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using Languages.Implementation;
+using Languages.Interfaces;
+
+namespace StundenlohnBerechner
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            LoadTitleAndDescription();
+            InitializeLanguageManager();
+            LoadLanguagesToCombo();
+        }
+
+        private void buttonBerechne_Click(object sender, EventArgs e)
+        {
+            var wochenStunden = Convert.ToDouble(numericUpDownWochenstunden.Value);
+            var bruttoLohn = Convert.ToDouble(numericUpDownBruttomonatslohn.Value);
+            var ergebnis = Math.Round(bruttoLohn / (wochenStunden * 13 / 3), 2);
+            textBoxErgebnis.Text = ergebnis + @" €";
+        }
+
+        private void LoadTitleAndDescription()
+        {
+            Text = Application.ProductName + @" " + Application.ProductVersion;
+        }
+
+        private readonly ILanguageManager _lm = new LanguageManager();
+        private Language _lang;
+
+
+        private void InitializeLanguageManager()
+        {
+            _lm.SetCurrentLanguage("de-DE");
+            _lm.OnLanguageChanged += OnLanguageChanged;
+        }
+
+        private void LoadLanguagesToCombo()
+        {
+            foreach (var lang in _lm.GetLanguages())
+                comboBoxLanguage.Items.Add(lang.Name);
+            comboBoxLanguage.SelectedIndex = 0;
+        }
+
+        private void comboBoxLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxLanguage.SelectedItem.ToString())
+            {
+                case "Deutsch":
+                    _lm.SetCurrentLanguage("de-DE");
+                    break;
+                case "English (US)":
+                    _lm.SetCurrentLanguage("en-US");
+                    break;
+            }
+        }
+
+        private void OnLanguageChanged(object sender, EventArgs eventArgs)
+        {
+            _lang = _lm.GetCurrentLanguage();
+            labelWochenstunden.Text = _lang.GetWord("WeeklyHours");
+            labelBruttomonatslohn.Text = _lang.GetWord("GrossMonthlySalary");
+            buttonBerechne.Text = _lang.GetWord("Calculate");
+        }
+    }
+}
