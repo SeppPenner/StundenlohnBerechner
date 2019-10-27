bb13fd483ba5cd8df1600c3893bd070e17237f63
SeppPenner <HansM2013@gmx.de>
2019-10-27 22:22:11 +0100

    Updated nuget packages, added GitVersionTask.

--- StundenlohnBerechner/Properties/AssemblyInfo.cs
+++ StundenlohnBerechner/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 
 [assembly: Guid("24133ea8-7823-4ca4-9fd6-c6c8cd08da51")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
