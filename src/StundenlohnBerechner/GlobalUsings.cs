bba9c62c59edeaeff22d49d4fdd545761eadda36
SeppPenner <HansM2013@gmx.de>
2022-02-18 18:33:02 +0100

    Added nullable checks, added editorconfig, added file scoped namespaces, added global usings.

---
+++ src/StundenlohnBerechner/GlobalUsings.cs
@@ -0,0 +1,4 @@
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
+global using Languages.Implementation;
+global using Languages.Interfaces;
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
--- src/StundenlohnBerechner/Main.cs
+++ src/StundenlohnBerechner/Main.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Main.cs" company="Hämmer Electronics">
 //   Copyright (c) All rights reserved.
 // </copyright>
@@ -7,104 +7,104 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace StundenlohnBerechner
-{
-    using System;
-    using System.Windows.Forms;
+namespace StundenlohnBerechner;
 
-    using Languages.Implementation;
-    using Languages.Interfaces;
+/// <summary>
+/// The main form.
+/// </summary>
+public partial class Main : Form
+{
+    /// <summary>
+    /// The language manager.
+    /// </summary>
+    private readonly ILanguageManager languageManager = new LanguageManager();
 
     /// <summary>
-    /// The main form.
+    /// The language.
     /// </summary>
-    public partial class Main : Form
+    private ILanguage? language;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Main"/> class.
+    /// </summary>
+    public Main()
     {
-        /// <summary>
-        /// The language manager.
-        /// </summary>
-        private readonly ILanguageManager languageManager = new LanguageManager();
+        this.InitializeComponent();
+        this.LoadTitleAndDescription();
+        this.InitializeLanguageManager();
+        this.LoadLanguagesToCombo();
+    }
 
-        /// <summary>
-        /// The language.
-        /// </summary>
-        private ILanguage? language;
+    /// <summary>
+    /// Handles the calculate click.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The event args.</param>
+    private void ButtonCalculateClick(object sender, EventArgs e)
+    {
+        var weeklyHours = Convert.ToDouble(this.numericUpDownWochenstunden.Value);
+        var wage = Convert.ToDouble(this.numericUpDownBruttomonatslohn.Value);
+        var result = Math.Round(wage / (weeklyHours * 13 / 3), 2);
+        this.textBoxErgebnis.Text = result + @" €";
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Main"/> class.
-        /// </summary>
-        public Main()
+    /// <summary>
+    /// Loads the title and description.
+    /// </summary>
+    private void LoadTitleAndDescription()
+    {
+        this.Text = Application.ProductName + @" " + Application.ProductVersion;
+    }
+
+    /// <summary>
+    /// Initializes the language manager.
+    /// </summary>
+    private void InitializeLanguageManager()
+    {
+        this.languageManager.SetCurrentLanguage("de-DE");
+        this.languageManager.OnLanguageChanged += this.OnLanguageChanged;
+    }
+
+    /// <summary>
+    /// Loads the languages to the combo box.
+    /// </summary>
+    private void LoadLanguagesToCombo()
+    {
+        foreach (var lang in this.languageManager.GetLanguages())
         {
-            this.InitializeComponent();
-            this.LoadTitleAndDescription();
-            this.InitializeLanguageManager();
-            this.LoadLanguagesToCombo();
+            this.comboBoxLanguage.Items.Add(lang.Name);
         }
 
-        /// <summary>
-        /// Handles the calculate click.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The event args.</param>
-        private void ButtonCalculateClick(object sender, EventArgs e)
+        this.comboBoxLanguage.SelectedIndex = 0;
+    }
+
+    /// <summary>
+    /// Handles the language changed event.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The event args.</param>
+    private void ComboBoxLanguageSelectedIndexChanged(object sender, EventArgs e)
+    {
+        var selectedItem = this.comboBoxLanguage.SelectedItem.ToString();
+
+        if (string.IsNullOrEmpty(selectedItem))
         {
-            var weeklyHours = Convert.ToDouble(this.numericUpDownWochenstunden.Value);
-            var wage = Convert.ToDouble(this.numericUpDownBruttomonatslohn.Value);
-            var result = Math.Round(wage / (weeklyHours * 13 / 3), 2);
-            this.textBoxErgebnis.Text = result + @" €";
+            return;
         }
 
-        /// <summary>
-        /// Loads the title and description.
-        /// </summary>
-        private void LoadTitleAndDescription()
-        {
-            this.Text = Application.ProductName + @" " + Application.ProductVersion;
-        }
+        this.languageManager.SetCurrentLanguageFromName(selectedItem);
+    }
 
-        /// <summary>
-        /// Initializes the language manager.
-        /// </summary>
-        private void InitializeLanguageManager()
-        {
-            this.languageManager.SetCurrentLanguage("de-DE");
-            this.languageManager.OnLanguageChanged += this.OnLanguageChanged;
-        }
-
-        /// <summary>
-        /// Loads the languages to the combo box.
-        /// </summary>
-        private void LoadLanguagesToCombo()
-        {
-            foreach (var lang in this.languageManager.GetLanguages())
-            {
-                this.comboBoxLanguage.Items.Add(lang.Name);
-            }
-
-            this.comboBoxLanguage.SelectedIndex = 0;
-        }
-
-        /// <summary>
-        /// Handles the language changed event.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The event args.</param>
-        private void ComboBoxLanguageSelectedIndexChanged(object sender, EventArgs e)
-        {
-            this.languageManager.SetCurrentLanguageFromName(this.comboBoxLanguage.SelectedItem.ToString());
-        }
-
-        /// <summary>
-        /// Handles the language changed event.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The event args.</param>
-        private void OnLanguageChanged(object sender, EventArgs e)
-        {
-            this.language = this.languageManager.GetCurrentLanguage();
-            this.labelWochenstunden.Text = this.language.GetWord("WeeklyHours");
-            this.labelBruttomonatslohn.Text = this.language.GetWord("GrossMonthlySalary");
-            this.buttonBerechne.Text = this.language.GetWord("Calculate");
-        }
+    /// <summary>
+    /// Handles the language changed event.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The event args.</param>
+    private void OnLanguageChanged(object sender, EventArgs e)
+    {
+        this.language = this.languageManager.GetCurrentLanguage();
+        this.labelWochenstunden.Text = this.language.GetWord("WeeklyHours");
+        this.labelBruttomonatslohn.Text = this.language.GetWord("GrossMonthlySalary");
+        this.buttonBerechne.Text = this.language.GetWord("Calculate");
     }
 }
