aab866b18587c78bdb46539d596ceba08a7aa6c1
SeppPenner <HansM2013@gmx.de>
2024-01-04 21:52:21 +0100

    Updated nuget packages, moved to Net 8.0.

--- src/StundenlohnBerechner/Main.cs
+++ src/StundenlohnBerechner/Main.cs
@@ -85,7 +85,7 @@ public partial class Main : Form
     /// <param name="e">The event args.</param>
     private void ComboBoxLanguageSelectedIndexChanged(object sender, EventArgs e)
     {
-        var selectedItem = this.comboBoxLanguage.SelectedItem.ToString();
+        var selectedItem = this.comboBoxLanguage.SelectedItem?.ToString();
 
         if (string.IsNullOrEmpty(selectedItem))
         {
